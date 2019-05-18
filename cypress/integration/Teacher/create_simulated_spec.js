821d925a9bb23723316d36d71248914b85b60488
RafaBarros93 <you@example.com>
2019-05-18 17:11:01 -0300

    Novos comandos

--- cypress/integration/Teacher/create_simulated_spec.js
+++ cypress/integration/Teacher/create_simulated_spec.js
@@ -17,15 +17,19 @@ describe("Login", () => {
             "E-mail ou senha inválidos."
         );
     });
-
     it("email completed and blank password", () => {
         cy.login("fael1-6@hotmail.com");
 
-        /* cy.get("input")
-            .contains("Entrar")
-            .click(); */
-
         cy.containsClick("input", "Entrar");
+
+        cy.get(".alert-messages").should(
+            "contain",
+            "E-mail ou senha inválidos."
+        );
+    });
+
+    it("email and invalid passwords", () => {
+        cy.login("teste@invalido.com", "123456");
 
         cy.get(".alert-messages").should(
             "contain",
@@ -35,12 +39,24 @@ describe("Login", () => {
 });
 
 describe("Create Simulated", () => {
+    const number = Math.floor(Math.random() * (500 - 2 + 1) + 3);
+
+    console.log(number);
+
     beforeEach(() => {
         cy.visit("/teachers/sign_in");
         cy.login("fael1-6@hotmail.com", "321321321");
     });
 
     it("create new simulete", () => {
-        cy.pageCreateSimulete();
+        cy.createSimulete(`TESTE QA AUTOMATIZAÇÃO ${number} `);
+    });
+
+    it("publish draft", () => {
+        cy.publishDummy(`TESTE QA AUTOMATIZAÇÃO ${number}`);
+    });
+
+    it("publish draft with error on date", () => {
+        cy.publishDummyWithErrorOnDate("TESTE QA AUTOMATIZAÇÃO 01");
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,7 +7,7 @@
 Cypress.Commands.add("containsClick", (type, contains) => {
     cy.get(type)
         .contains(contains)
-        .click();
+        .click({ force: true });
 });
 
 Cypress.Commands.add("login", (login, password) => {
@@ -26,19 +26,161 @@ Cypress.Commands.add("login", (login, password) => {
     }
 });
 
-Cypress.Commands.add("pageCreateSimulete", () => {
+Cypress.Commands.add("saveAsDraft", () => {
+    cy.get("#save_as_draft_button").click();
+
+    cy.testLocation(
+        "http://professor.processoseletivo.enade.saraivaeducacao.com.br/mocks"
+    );
+
+    cy.get(".toast").should("contain", "Simulado salvo como rascunho!");
+});
+
+Cypress.Commands.add(
+    "setHoursAndMinutes",
+    (start, element, hoursAndMinutes) => {
+        cy.get(start).click({ force: true });
+
+        cy.get(element)
+            .contains(hoursAndMinutes)
+            .click({ force: true });
+
+        cy.get("._hj-f5b2a1eb-9b07_widget_open_close").click({ force: true });
+    }
+);
+
+Cypress.Commands.add("setMonth", (today) => {
+    cy.get("#end_time__date").click();
+
+    cy.get(
+        "#end_time__date_root > .picker__holder > .picker__frame > .picker__wrap > .picker__box > .picker__calendar-container > .picker__header > .picker__nav--next"
+    ).click();
+
+    cy.get(
+        "#end_time__date_root > .picker__holder > .picker__frame > .picker__wrap > .picker__box > .picker__calendar-container"
+    )
+        .contains(today)
+        .click();
+
+    cy.get(
+        "#end_time__date_root > .picker__holder > .picker__frame > .picker__wrap > .picker__box > .picker__footer > .picker__close"
+    ).click();
+});
+
+Cypress.Commands.add("setStartTime", () => {
+    const todayHour = Cypress.moment().format("H");
+
+    let today = Cypress.moment().date();
+
+    cy.setHoursAndMinutes("#start_time__time", ".clockpicker-hours", todayHour);
+
+    cy.setHoursAndMinutes("#end_time__time", ".clockpicker-hours", todayHour);
+
+    cy.setMonth(today);
+});
+
+Cypress.Commands.add("setQuestion", (question) => {
+    for (let index = 1; index < question; index++) {
+        cy.get(
+            `#questions-list > :nth-child(${index}) > :nth-child(${1}) > .question-box > .question-box__actions-wrapper > .add-question`
+        ).click({ force: true });
+    }
+
+    cy.get("h2").should("contain", "Questões");
+
+    cy.get(".next-action > .waves-effect > .material-icons").click();
+
+    cy.saveAsDraft();
+});
+
+Cypress.Commands.add("setDiscipline", (type, discipline) => {
+    cy.containsClick(type, discipline);
+
+    cy.containsClick("label", "Selecionar todos");
+
+    cy.get("h2").should("contain", "Conteúdo");
+
+    cy.get(".next-action > .waves-effect > .material-icons").click();
+
+    cy.setQuestion(21);
+});
+
+Cypress.Commands.add("setNameSimulete", (message) => {
+    //  const message = "TESTE QA AUTOMATIZAÇÃO";
+
+    cy.get('input[name="mock[name]"]', { timeout: 150000 }).type(message);
+
+    // expect(message).to.be.equal("TESTE QA AUTOMATIZAÇÃO");
+
+    cy.containsClick('a[id="wizard-form__submit-button"]', "PRÓXIMO");
+
+    cy.setDiscipline(".subgroup-item", "Arquivologia");
+});
+
+Cypress.Commands.add("createSimulete", (name) => {
     cy.containsClick("span", "Simulados");
 
-    cy.location().should((loc) => {
-        expect(loc.href).to.eq(
-            "http://professor.processoseletivo.enade.saraivaeducacao.com.br/mocks"
-        );
-
-        expect(loc.protocol).to.eq("http:");
-        expect(loc.toString()).to.eq(
-            "http://professor.processoseletivo.enade.saraivaeducacao.com.br/mocks"
-        );
-    });
+    cy.testLocation(
+        "http://professor.processoseletivo.enade.saraivaeducacao.com.br/mocks"
+    );
 
     cy.containsClick("span", "Novo simulado");
+
+    cy.setNameSimulete(name);
+});
+
+Cypress.Commands.add("searchDummy", (numberDummy, name) => {
+    console.log(numberDummy, name);
+
+    cy.containsClick("span", "Simulados");
+
+    cy.get("#q_name_cont", { timeout: 150000 }).type(name);
+
+    cy.get(".search").click();
+
+    // cy.get(` [title="${name}"]`).click();
+
+    cy.get(".long-text > .primary-color").click();
+});
+
+Cypress.Commands.add("publish", () => {
+    cy.get("#publish-button").click();
+
+    cy.containsClick("#publish-modal > .modal-footer", "OK");
+});
+
+Cypress.Commands.add("publishDummy", (name) => {
+    cy.get("#sidebar-mocks > span").click();
+
+    cy.searchDummy(1, name);
+
+    cy.get("#edit-button").click();
+
+    cy.setStartTime();
+
+    cy.get("#publish_button").click({ force: true });
+
+    cy.get(".toast").should("contain", "Simulado publicado com sucesso.");
+});
+
+Cypress.Commands.add("publishDummyWithErrorOnDate", (name) => {
+    cy.searchDummy(1, name);
+
+    cy.publish();
+
+    cy.get("#publish-modal > .modal-content").should("contain", "ATENÇÃO!");
+
+    cy.get("#publish-notice__modal > .modal-content").should(
+        "contain",
+        "Simulado não pode ser publicado:"
+    );
+});
+
+Cypress.Commands.add("testLocation", (location) => {
+    cy.location().should((loc) => {
+        expect(loc.href).to.eq(location);
+
+        expect(loc.protocol).to.eq("http:");
+        expect(loc.toString()).to.eq(location);
+    });
 });
